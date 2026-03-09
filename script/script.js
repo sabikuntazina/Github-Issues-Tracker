5188c445d81cd4551a3a6ec9ffb40f460d080adf
sabikuntazina2025 <sabikuntazina@gmail.com>
2026-03-09 11:01:03 +0600

    All Cards are added by JS

---
+++ script/script.js
@@ -0,0 +1,69 @@
+const allTab = document.getElementById("allTab");
+const openTab = document.getElementById("openTab");
+const closeTab = document.getElementById("closedTab");
+let allIssues = [];
+
+const loadAllIssues = () => {
+  fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then((res) => res.json())
+    .then((json) => {
+      allIssues = json.data;
+      displayAllIssues(allIssues);
+    });
+};
+// "id": 1,
+// "title": "Fix navigation menu on mobile devices",
+// "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+// "status": "open",
+// "labels": [
+// "bug",
+// "help wanted"
+// ],
+// "priority": "high",
+// "author": "john_doe",
+// "assignee": "jane_smith",
+// "createdAt": "2024-01-15T10:30:00Z",
+// "updatedAt": "2024-01-15T10:30:00Z"
+
+const displayAllIssues = (issues) => {
+ 
+  const issueContainer = document.getElementById("issue-container");
+  issueContainer.innerHTML = "";
+  
+  issues.forEach((issue) => {
+    
+    const card = document.createElement("div");
+    card.innerHTML = `
+    
+        <div class="space-y-4 h-full shadow bg-white py-8 px-4 rounded-md border-t-6 ${issue.status==="open" ? "border-green-500": "border-purple-600" }">
+            <div class="flex justify-between items-center">  ${issue.status==="open" ? '<img src="./assets/Open-Status.png" alt="">':'<img src="./assets/Closed- Status .png" alt="">'  }
+              
+              <button class="btn btn-soft ${
+                issue.priority === "high"
+                  ? "btn-error border-red-300"
+                  : issue.priority === "medium"
+                    ? "btn-warning border-yellow-300"
+                    : "bg-gray-300 text-gray-500s  border-gray-400"
+              } rounded-xl border-2 ">${issue.priority} </button>
+            </div>
+            <h2 class="text-lg font-bold text-neutral-950">${issue.title} </h2>
+            <p class="text-gray-500 text-sm line-clamp-2 ">
+              ${issue.description}
+            </p>
+            <div class="space-x-4 my-6">
+              <button class="btn btn-soft btn-error rounded-xl border-2 border-red-300"><i class="fa-solid fa-bug"></i> Bug</button>
+              <button class="btn btn-soft btn-warning border-2 rounded-xl border-yellow-200">help wanted</button>
+            </div>
+            <hr />
+             <p class="text-gray-500 text-sm">
+          #${issue.id} by ${issue.author}
+        </p>
+            <p class="text-gray-500 text-sm ">${issue.updatedAt} </p>
+          </div>
+        `;
+    issueContainer.append(card);
+  });
+};
+
+
+// id="lesson-btn-${issueTab.level_no}" onclick="loadLevelWord(${issueTab.status})"
